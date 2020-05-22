5b020e2b5b460dfcdde9b675af56f4e78bf6e474
dantorl <daniel.torquato@hotmail.com>
2020-05-22 17:09:47 +0000

    Delete Questionario.java

--- src/main/java/com/recomendacao/investimento/repositories/Questionario.java
+++
@@ -1,6 +0,0 @@
-package com.recomendacao.investimento.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface Questionario extends CrudRepository<Questionario, Integer> {
-}
