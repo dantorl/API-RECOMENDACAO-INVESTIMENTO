e865fd23a079ec01ab467e92814d35caa13e32a0
Fellipe Roveri <roverifellipe@gmail.com>
2020-05-27 10:50:21 -0300

    Ajustar classe de testes InvestidorServiceTests

--- src/test/java/com/recomendacao/investimento/services/InvestidorServiceTests.java
+++ src/test/java/com/recomendacao/investimento/services/InvestidorServiceTests.java
@@ -12,6 +12,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
 import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
 
 @SpringBootTest
 public class InvestidorServiceTests {
@@ -45,4 +47,44 @@ public class InvestidorServiceTests {
         Assertions.assertEquals(investidor.getEmail(), investidorObjeto.getEmail());
 
     }
+
+    @Test
+    public void testarAtualizarInvestidor(){
+
+        investidor.setEmail("novoemailservico@gmail.com");
+
+        Mockito.when(investidorRepository.save(Mockito.any(Investidor.class))).thenReturn(investidor);
+
+        Investidor investidorObjeto = investidorService.atualizarInvestidor(investidor);
+
+        Assertions.assertEquals(investidor.getEmail(), investidorObjeto.getEmail());
+    }
+
+    @Test
+    public void testarBuscarInvestidorPorId(){
+
+        Optional<Investidor> optionalLead = Optional.of(investidor);
+
+        Mockito.when(investidorRepository.findById(Mockito.any(Integer.class))).thenReturn(optionalLead);
+
+        Assertions.assertEquals(investidor, optionalLead.get());
+    }
+
+    @Test
+    public void testarBuscaTodosInvestidores(){
+
+        Iterable<Investidor> investidorIterable = Arrays.asList(investidor);
+
+        Mockito.when(investidorService.buscarTodosInvestidores()).thenReturn(investidorIterable);
+
+        Assertions.assertEquals(investidor, ((List<Investidor>) investidorIterable).get(0));
+    }
+
+
+    @Test
+    public void testarDeletarInvestidor(){
+        investidorService.deletarInvestidor(investidor);
+        Mockito.verify(investidorRepository).delete(Mockito.any(Investidor.class));
+    }
+
 }
